4d76936f8ed7b4a57d247298853daa06f0128bc1
kclenuar <kid778@hotmail.co.uk>
2020-01-27 16:56:03 +0000

    Add generated libgdx project.

---
+++ desktop/src/kclenuar/game1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kclenuar.game1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kclenuar.game1.Game1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game1(), config);
+	}
+}
